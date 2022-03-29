7e6a679061ef58b9834519e86aa16fd217f342eb
sboursen <dev.sboursen@gmail.com>
2022-03-29 11:09:36 +0100

    Display books from the Redux store using useSelector

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Categories from './routes/categories/categories';
 import NoMatch from './routes/no-match/no-match';
 import Header from './components/header/header';
 
-
 export default function App() {
   return (
     <>
--- src/routes/books/books.js
+++ src/routes/books/books.js
@@ -1,31 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookCard from '../../components/book-card/book-card';
 import Form from '../../components/form/from';
 
 export default function Books() {
+  const books = useSelector((store) => store.books);
+
+  const bookList = books.map((book) => {
+    const {
+      id, chapter, percent, genre, title, author,
+    } = book;
+
+    return (
+      <BookCard
+        key={id}
+        chapter={chapter}
+        percent={percent}
+        genre={genre}
+        title={title}
+        author={author}
+      />
+    );
+  });
+
   return (
     <main className="flex flex-col mb-24 bg-[#fafafa]">
-      <BookCard
-        chapter="Chapter 17"
-        percent={64}
-        genre="Action"
-        title="The Hunger Games"
-        author="Suzanne Collins"
-      />
-      <BookCard
-        chapter='Chapter 3: "A Lesson Learned "'
-        percent={8}
-        genre="Science Fiction"
-        title="Dune"
-        author="Frank Herbert"
-      />
-      <BookCard
-        chapter="Introduction"
-        percent={0}
-        genre="Economy"
-        title="Capital in the Twenty-First Century"
-        author="Suzanne Collins"
-      />
+      {bookList}
       <Form />
     </main>
   );
