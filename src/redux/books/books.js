609e78b0c3ad7997926c670d19954372391fbbf1
sboursen <dev.sboursen@gmail.com>
2022-03-29 10:51:02 +0100

    Test redux books using the new initial state

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,33 @@
-const defaultState = [];
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookstore-cms/books/ADD';
 const REMOVE_BOOK = 'bookstore-cms/books/REMOVE';
+const book1 = {
+  id: uuidv4(),
+  chapter: 'Chapter 17',
+  percent: 64,
+  genre: 'Action',
+  title: 'The Hunger Games',
+  author: 'Suzanne Collins',
+};
+const book2 = {
+  id: uuidv4(),
+  chapter: 'Chapter 17',
+  percent: 8,
+  genre: 'Science Fiction',
+  title: 'Dune',
+  author: 'Frank Herbert',
+};
+const book3 = {
+  id: uuidv4(),
+  chapter: 'Introduction',
+  percent: 0,
+  genre: 'Economy',
+  title: 'Capital in the Twenty-First Century',
+  author: 'Suzanne Collins',
+};
+
+const defaultState = [book1, book2, book3];
 
 export function addBook(book) {
   return { type: ADD_BOOK, book };
