96fbe4c38c24653413be87aa7605e64356489b61
sboursen <dev.sboursen@gmail.com>
2022-03-30 11:52:05 +0100

    Add fetch error handling

--- src/api/bookstore-api.js
+++ src/api/bookstore-api.js
@@ -14,6 +14,11 @@ export default class BookstoreApi {
         'Content-Type': 'application/json',
       },
     });
+    if (response.status !== 200) {
+      throw new Error(
+        'Can not fetch the books with the provided Endpoint',
+      );
+    }
     const data = await response.json;
     return data;
   }
@@ -27,24 +32,36 @@ export default class BookstoreApi {
       category,
     };
 
-    fetch(booksEndpoint, {
+    const response = await fetch(booksEndpoint, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     });
+
+    if (response.status !== 200) {
+      throw new Error(
+        `Can not add the book with the id: ${itemId}`,
+      );
+    }
   }
 
   static async deleteBook(itemId) {
     const bookDeleteEndpoint = `${this.baseURL}/${this.appId}/books/${itemId}`;
 
-    fetch(bookDeleteEndpoint, {
+    const response = await fetch(bookDeleteEndpoint, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
       },
       body: 'false',
     });
+
+    if (response.status !== 200) {
+      throw new Error(
+        `Can not delete the book with the id: ${itemId}`,
+      );
+    }
   }
 }
