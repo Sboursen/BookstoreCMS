b244420f53fe89bc26a49ffd0fbd7f687a17afcb
sboursen <dev.sboursen@gmail.com>
2022-03-31 09:49:17 +0100

    Add the theme fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,19 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      azure: '#0290ff',
-      'black-two': '#121212',
-      'cool-blue': '#4386bf',
-      'warm-grey': '#888888',
-      'pinkish-grey': '#c4c4c4',
-      'dirty-white': '#fafafa',
+      colors: {
+        azure: '#0290ff',
+        'black-two': '#121212',
+        'cool-blue': '#4386bf',
+        'warm-grey': '#888888',
+        'pinkish-grey': '#c4c4c4',
+        'dirty-white': '#fafafa',
+      },
     },
+  },
+  fontFamily: {
+    montserrat: ['Montserrat', 'sans-serif'],
+    roboto: ['"Roboto Slab"', 'serif'],
   },
   plugins: [],
 };
